6762242c0b75bb82514feca8fd7e0927b4da3070
tomekmazurek <mazursky.t@gmail.com>
2021-05-23 23:20:16 +0200

    feat: add writing to xml file functionality

--- src/test/java/com/github/tomekmazurek/songsapp/filehandling/XmlFileHandlerTest.java
+++ src/test/java/com/github/tomekmazurek/songsapp/filehandling/XmlFileHandlerTest.java
@@ -1,15 +1,21 @@
 package com.github.tomekmazurek.songsapp.filehandling;
 
 import com.github.tomekmazurek.songsapp.dto.SongDto;
+import com.github.tomekmazurek.songsapp.dto.SongsMother;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class XmlFileHandlerTest {
   @Test
-   void shouldReadDataFromFile() {
+  void shouldReadDataFromFile() {
     // given
     MockMultipartFile mockFile = FileHandlerMother.getXmlTestFile();
 
@@ -21,5 +27,26 @@ class XmlFileHandlerTest {
     Assertions.assertThat(songs).isNotEmpty();
     Assertions.assertThat(songs.size()).isEqualTo(3);
     Assertions.assertThat(songs.get(0).getTitle()).isEqualTo("Living in a Ghost Town");
+  }
+
+  @Test
+  void shouldWriteDataToXmlFileTest() throws IOException {
+    // given
+    List<SongDto> songs = new ArrayList<>();
+    songs.add(SongsMother.createBackInBlackSongDto());
+    XmlFileHandler handler = new XmlFileHandler();
+
+    // when
+    File outputFile = handler.writeToFile(songs);
+
+    // then
+    BufferedReader reader = new BufferedReader(new FileReader("report.xml"));
+    Assertions.assertThat(outputFile).exists().isNotEmpty();
+    Assertions.assertThat(reader.lines().collect(Collectors.joining()))
+        .contains("<songs>")
+        .contains("<song>")
+        .contains("<title>Back in Black</title");
+    reader.close();
+    Files.delete(Path.of("report.xml"));
   }
 }
