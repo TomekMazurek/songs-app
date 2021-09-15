919640be4b543bdd8a548fa87d4763b3894df5fa
tomekmazurek <mazursky.t@gmail.com>
2021-09-15 18:15:11 +0200

    feat: edited Swagger documentation, edited SongService, getSongs returns now list of songs sorted by title

--- src/main/java/com/github/tomekmazurek/songsapp/controller/SongController.java
+++ src/main/java/com/github/tomekmazurek/songsapp/controller/SongController.java
@@ -3,14 +3,17 @@ package com.github.tomekmazurek.songsapp.controller;
 import com.github.tomekmazurek.songsapp.dto.CategoryDto;
 import com.github.tomekmazurek.songsapp.dto.SongDto;
 import com.github.tomekmazurek.songsapp.service.SongService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@Api(value = "Songs API", tags = "Song API", produces = "application/json")
 @RestController
-@EnableSwagger2
 @CrossOrigin(origins = "*")
 @RequestMapping("api/v1/songs")
 public class SongController {
@@ -21,41 +24,59 @@ public class SongController {
         this.songService = songService;
     }
 
+    @ApiOperation(value = "get List of categories", response = CategoryDto[].class, produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list of songs")
+    })
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryDto>> getCategories() {
         return ResponseEntity.ok(songService.getCategories());
     }
 
+    @ApiOperation(value = "add song to database", response = SongDto.class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success")})
     @PostMapping
     public ResponseEntity<SongDto> addSong(@RequestBody SongDto songDto) {
         return ResponseEntity.ok(songService.addSong(songDto));
     }
 
+    @ApiOperation(value = "get List of Songs", response = SongDto[].class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success")})
     @GetMapping
     public ResponseEntity<List<SongDto>> getSongs() {
         return ResponseEntity.ok(songService.getSongs());
     }
 
+    @ApiOperation(value = "get song with given id", response = SongDto.class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success")})
     @GetMapping("/{id}")
     public ResponseEntity<SongDto> getSong(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(songService.getSingleSong(id));
     }
 
+    @ApiOperation(value = "increment number of votes for song with given id", response = SongDto.class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success")})
     @PutMapping("/votes/{id}")
     public ResponseEntity<SongDto> voteForSong(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(songService.voteForSong(id));
     }
 
+    @ApiOperation(value = "clear votes for all songs in db", response = SongDto[].class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success")})
     @PutMapping("/votes")
     public ResponseEntity<List<SongDto>> clearAllVotes() {
         return ResponseEntity.ok(songService.clearAllVotes());
     }
 
+    @ApiOperation(value = "clear votes for song with given id", response = SongDto.class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success")})
     @PutMapping("/votes/{id}/clear")
     public ResponseEntity<SongDto> clearVotesForSong(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(songService.clearVotes(id));
     }
 
+    @ApiOperation(value = "update data for song", response = SongDto.class, produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success")})
     @PutMapping("/{id}")
     public ResponseEntity<SongDto> updateSong(@PathVariable(name = "id") Long id, @RequestBody SongDto songDto) {
         return ResponseEntity.ok(songService.updateSong(id, songDto));
--- src/main/java/com/github/tomekmazurek/songsapp/dto/SongDto.java
+++ src/main/java/com/github/tomekmazurek/songsapp/dto/SongDto.java
@@ -3,6 +3,8 @@ package com.github.tomekmazurek.songsapp.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.opencsv.bean.CsvBindByName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
@@ -12,56 +14,59 @@ import org.simpleframework.xml.Root;
 @NoArgsConstructor
 @Builder
 @Root(name = "song")
+@ApiModel(value = "BookDto")
 public class SongDto {
-  Long id;
 
-  @CsvBindByName
-  @Element(name = "title")
-  String title;
+    @ApiModelProperty(value = "id", dataType = "Long", example = "4")
+    Long id;
+    @ApiModelProperty(value = "title", dataType = "String", example = "Imagine")
+    @CsvBindByName
+    @Element(name = "title")
+    String title;
+    @ApiModelProperty(value = "author", dataType = "String", example = "John Lennon")
+    @CsvBindByName
+    @Element(name = "author")
+    String author;
+    @ApiModelProperty(value = "album", dataType = "String", example = "Imagine")
+    @CsvBindByName
+    @Element(name = "album")
+    String album;
+    @ApiModelProperty(value = "category", dataType = "String", example = "Pop")
+    @CsvBindByName
+    @Element(name = "category")
+    String category;
+    @ApiModelProperty(value = "votes", dataType = "int", example = "1")
+    @CsvBindByName
+    @Element(name = "votes")
+    int votes;
 
-  @CsvBindByName
-  @Element(name = "author")
-  String author;
+    @JsonCreator
+    public SongDto(
+            @JsonProperty("id") Long id,
+            @JsonProperty("title") String title,
+            @JsonProperty("author") String author,
+            @JsonProperty("album") String album,
+            @JsonProperty("category") String category,
+            @JsonProperty("votes") int votes) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.album = album;
+        this.category = category;
+        this.votes = votes;
+    }
 
-  @CsvBindByName
-  @Element(name = "album")
-  String album;
-
-  @CsvBindByName
-  @Element(name = "category")
-  String category;
-
-  @CsvBindByName
-  @Element(name = "votes")
-  int votes;
-
-  @JsonCreator
-  public SongDto(
-      @JsonProperty("id") Long id,
-      @JsonProperty("title") String title,
-      @JsonProperty("author") String author,
-      @JsonProperty("album") String album,
-      @JsonProperty("category") String category,
-      @JsonProperty("votes") int votes) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.album = album;
-    this.category = category;
-    this.votes = votes;
-  }
-
-  @JsonCreator
-  public SongDto(
-      @JsonProperty("title") String title,
-      @JsonProperty("author") String author,
-      @JsonProperty("album") String album,
-      @JsonProperty("category") String category,
-      @JsonProperty("votes") int votes) {
-    this.title = title;
-    this.author = author;
-    this.album = album;
-    this.category = category;
-    this.votes = votes;
-  }
+    @JsonCreator
+    public SongDto(
+            @JsonProperty("title") String title,
+            @JsonProperty("author") String author,
+            @JsonProperty("album") String album,
+            @JsonProperty("category") String category,
+            @JsonProperty("votes") int votes) {
+        this.title = title;
+        this.author = author;
+        this.album = album;
+        this.category = category;
+        this.votes = votes;
+    }
 }
